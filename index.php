affd6f25a3800f2d852cad5c93a6600c4bcb74aa
zxyctn <zxyctn@gmail.com>
2019-03-03 23:03:42 +0300

    Create index.php
    Created a temporary index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Eventually</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body class="bg-secondary">
+    <div class="container">
+        <div class="jumbotron p-5 m-5">
+            <h1 class="display-4">Coming soon...</h1>
+            <p class="lead">Checkout our GitHub page to see the status</p>
+            <a class="btn btn-primary btn-lg" href="https://github.com/zxyctn/Eventually">Learn more</a>
+        </div>
+    </div>
+</body>
+</html>
