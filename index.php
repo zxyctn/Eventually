9bb27f0a8a42c6db2bd4e033f3e3c99d63387782
zxyctn <zxyctn@gmail.com>
2019-04-30 01:20:27 +0300

    Index page renewal

--- index.php
+++ index.php
@@ -1,19 +1,113 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Eventually</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body class="bg-secondary">
-    <div class="container">
-        <div class="jumbotron p-5 m-5">
-            <h1 class="display-4">Coming soon...</h1>
-            <p class="lead">Checkout our GitHub page to see the status</p>
-            <a class="btn btn-primary btn-lg" href="https://github.com/zxyctn/Eventually">Learn more</a>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-around">
+        <ul class="navbar-nav align-items-center">
+            <li class="nav-item px-3">
+                <a href="#" class="nav-link">EXPLORE</a>
+            </li>
+            <li class="nav-item px-3">
+                <a href="#" class="nav-link">GROUPS</a>
+            </li>
+            <li class="nav-item px-2">
+                <a href="#"><img src="img/eventually_logo_cropped.png" height="30"></a>
+            </li>
+            <li class="nav-item px-2">
+                <a href="port.html" class="nav-link">CONTACT</a>
+            </li>
+            <li class="nav-item px-3">
+                <a href="#" class="nav-link">ABOUT</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="jumbotron jumbotron-fluid bg-white">
+        <div class="container text-center">
+            <h1 class="display-4">Search</h1>
+            <p class="lead">Find events, groups and connect with people</p>
+            <form class="form-inline justify-content-center">
+                <input class="form-control mr-sm-2 d-flex col-6" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-primary" type="submit">Search</button>
+            </form>
         </div>
     </div>
+
+    <section class="bg-primary text-white p-5 text-center">
+        <h1 class="display-4">Welcome</h1>
+        <p class="lead p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et minima veniam enim aliquam
+            voluptatibus vel
+            repellat, aperiam laudantium! Corrupti nobis eius, alias atque nostrum consectetur laudantium doloremque
+            odio aperiam quis id officiis assumenda laborum minima accusantium sit recusandae ducimus provident!
+            Perferendis, odit, tenetur deserunt, voluptas quam rerum vero accusantium ipsa voluptate architecto quas
+            necessitatibus hic laboriosam autem? Praesentium soluta earum ratione sint accusantium officiis esse sed
+            corporis eligendi culpa quasi optio, dolore pariatur deserunt aliquam deleniti cupiditate tempore similique
+            laudantium rem nisi temporibus, nulla est eius. Exercitationem maiores voluptates amet molestias illum odit
+            nulla dolores explicabo esse delectus, natus optio eaque fugit ipsum veritatis. Quo qui quas maxime ipsam
+            pariatur minima suscipit doloremque cum quod accusamus ab sit necessitatibus animi earum odio repellendus,
+            est nesciunt adipisci esse, fuga, similique expedita? Cum, quibusdam deserunt, consectetur possimus saepe,
+            repellat molestias minima error tempora nihil labore. Fugit recusandae excepturi deleniti repudiandae.
+        </p>
+        <div class="inline-block">
+            <a class="btn btn-danger" href="#">Sign Up</a>
+            <a class="btn btn-success" href="#">Sign In</a>
+        </div>
+    </section>
+
+    <footer class="bg-dark">
+        <div class="d-flex text-white p-3 justify-content-center">
+            <div class="p-3 col-4 d-flex">
+                <div class="container align-items-center">
+                    <a href="#"><img src="img/eventually_logo_cropped.png" height="30"></a>
+                    <p class="lead" style="font-size: 11pt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nam
+                        repellendus
+                        nobis consequuntur sed!
+                        Nam et nostrum, suscipit quia possimus consequatur ducimus maiores doloribus in officia
+                        accusamus?
+                        Qui, error consectetur?</p>
+                    <a href="#"><img src="img/github.png" height="30"></a>
+                </div>
+                <div class="my-3" style="background: white; height: inherit; width: 0.1rem"></div>
+            </div>
+            <div class="p-3 d-flex inline-block col-3 justify-content-around align-items-center">
+                <div>
+                    <h1 class="lead" style="font-size: 14pt"><strong>Category</strong></h1>
+                    <h1 class="lead" style="font-size: 11pt">Music</h1>
+                    <h1 class="lead" style="font-size: 11pt">Dance</h1>
+                    <h1 class="lead" style="font-size: 11pt">Books</h1>
+                    <h1 class="lead" style="font-size: 11pt">Travel</h1>
+                </div>
+                <div>
+                    <h1 class="lead" style="font-size: 14pt"><strong>Site Map</strong></h1>
+                    <h1 class="lead" style="font-size: 11pt">Explore</h1>
+                    <h1 class="lead" style="font-size: 11pt">Groups</h1>
+                    <h1 class="lead" style="font-size: 11pt">About Us</h1>
+                    <h1 class="lead" style="font-size: 11pt">Contact</h1>
+                </div>
+            </div>
+            <div class="p-3 d-flex col-4 ">
+                <div class="my-3" style="background: white; height: inherit; width: 0.1rem"></div>
+                <p class="px-3 d-flex align-items-end" style="font-size: 11pt">Copyright (C) 2019</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
